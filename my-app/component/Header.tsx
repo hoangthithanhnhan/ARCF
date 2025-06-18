ef7bee2ebfdc60e32d10b9c48ba62b938e86ff8f
Hoàng Thị Thanh Nhàn <nhan95dd2017@gmail.com>
2025-06-18 18:00:46 +0700

    Update Header.tsx

--- my-app/component/Header.tsx
+++ my-app/component/Header.tsx
@@ -5,17 +5,17 @@ import Image from 'next/image'
 const Header = () => {
   return (
     <header className="py-[40px] px-[72px]">
-      <nav className="max-w-7xl mx-auto flex items-center justify-between">
+      <nav className="flex items-center justify-between">
         <Link href="" className="flex-shrink-0">
           <Image src="/logo.png" alt="Logo" width={306} height={76}/>
         </Link> 
         <div className="hidden lg:flex justify-center flex-1">
-          <div className="flex space-x-[54px]">
+          <div className="flex space-x-13.5 text-6xl">
             {["About", "How We Give", "Donations & Support", "News", "Contact"].map((item) => (
               <Link
                 key={item}
                 href="#"
-                className="text-[1.25rem] font-semibold text-[var(--body)]"
+                className="text-xl font-semibold text-[var(--body)]"
               >
                 {item}
               </Link>
@@ -25,7 +25,7 @@ const Header = () => {
         <div>
           <Link
             href="#"
-            className="bg-[var(--green)] text-[var(--body)] py-[13px] px-[50px] rounded-full font-semibold text-[1.25rem] no-underline"
+            className="bg-[var(--green)] text-[var(--body)] py-3.5 px-[50px] rounded-full font-semibold text-[1.25rem] no-underline"
           >
             Donate
           </Link>
